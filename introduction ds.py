123f3742546af22e7de3aac22f4331898150575f
SeepathiSaiRaj <136920677+SeepathiSaiRaj@users.noreply.github.com>
2024-05-20 11:19:21 +0530

    Add files via upload

---
+++ introduction ds.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+exercise 2
+#INTRODUCTION TO DATA SCIENCE
+
+
+# In[ ]:
+
+
+exercise 3
+ Data science helps us collect the pieces (data), clean and organize them, and find patterns to solve the puzzle (gain insights).  This lets us understand what happened, why, and even predict the future!
+    
+
+
+# In[ ]:
+
+
+exercise 4
+python 
+R
+SQL
+
+
+# In[ ]:
+
+
+exercise 5
+pandas 
+matplotlib
+numpy
+
+
+# In[ ]:
+
+
+exercise 6
+jupyter notebook
+watson studio
+sikit learn
+tensor flow
+
+
+# In[7]:
+
+
+#exercise 7
+(2+3),(2*4),(6%8)
+
+
+# In[13]:
+
+
+#exercise 8
+# This code multiplies and adds numbers 
+product = 5 * 3
+sum = product + 17
+product, sum
+
+
+# In[17]:
+
+
+#exercise 9
+# This code converts minutes to hours
+minutes = 120
+hours = minutes / 60
+print(f"{minutes} minutes is equal to {hours} hours")
+
+
+# In[ ]:
+
+
+#exercise 10
+extract knowledge from data
+solve real world problems
+identify patterns-trends
+
+
+# In[ ]:
+
+
+#exercise 11
+AUTHOR
+Sai Raj
+
